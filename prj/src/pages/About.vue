7003ee570b17b2b12e26cd21fcd34017cf0b05d5
林龙辉 <540886643@qq.com>
2016-12-14 16:17:32 +0800

    * publish

--- prj/src/pages/About.vue
+++ prj/src/pages/About.vue
@@ -1,46 +1,11 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    <div class="user" v-if="user">
-      <img :src="user.avatar_url">
-      <p>hello, i am {{ user.name }}</p>
-      <p>github: <a :href="user.html_url">{{ user.login }}</a></p>
-    </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import * as apis from 'src/apis/index'
-
 export default {
-  name: 'about',
-  data () {
-    return {
-      user: null
-    }
-  },
-  mounted () {
-    this.setTitle('about')
-    this.getUser()
-  },
-  methods: {
-    async getUser () {
-      let data = await apis.get(`${apis.GET_USER_URL}/funkyLover`, {}, true)
-      if (data.message) {
-        // req error
-        alert(data.message)
-      } else {
-        this.user = data
-      }
-    },
-    ...mapActions([ 'setTitle' ])
-  }
+  name: 'about'
 }
 </script>
-
-<style scoped>
-img {
-  width: 150px;
-}
-</style>
